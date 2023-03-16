d285dbdedc463151f6bf6eb956c24b06a5957c62
ssups <shalstlqruki@gmail.com>
2023-03-17 02:21:25 +0900

    delete artifacts folder

--- blockchain/contracts/TestToken.sol
+++ blockchain/contracts/TestToken.sol
@@ -3,8 +3,9 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract TestToken is ERC721 {
+contract TestToken is ERC721Enumerable {
     uint256 public price;
     string private _baseTokenURI = "http://test.baseURI/";
 
--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,6 +1,12 @@
 /** @type import('hardhat/config').HardhatUserConfig */
-require('@nomiclabs/hardhat-waffle');
+// require('@nomiclabs/hardhat-waffle');
+// test코드에서 solidity int 리턴이 bigInt로 되있어서 비교가 힘들었음
+// 그래서 waffle 에서 toolbox로 바꿈 공식문서에서도 추천
+// https://hardhat.org/hardhat-runner/docs/guides/migrating-from-hardhat-waffle
+require('@nomicfoundation/hardhat-toolbox');
+// https://hardhat.org/hardhat-chai-matchers/docs/overview
+require('@nomicfoundation/hardhat-chai-matchers');
 
 module.exports = {
-  solidity: '0.8.1',
+  solidity: '0.8.18',
 };
