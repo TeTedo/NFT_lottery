0f1752b781d929906c3fe4c278fa2a0367b0df35
ssups <shalstlqruki@gmail.com>
2023-03-21 21:33:54 +0900

    fix Contract_GetNftData: change interacting contract to Interface format

--- blockchain/contracts/GetNftData.sol
+++ blockchain/contracts/GetNftData.sol
@@ -2,40 +2,55 @@
 
 pragma solidity ^0.8.18;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/interfaces/IERC721Enumerable.sol";
+import "@openzeppelin/contracts/interfaces/IERC721Metadata.sol";
 
 contract GetNftData {
     function ownerOf(
-        address tokenCA,
-        uint256 tokenId
+        address _tokenCA,
+        uint256 _tokenId
     ) external view returns (address) {
-        ERC721Enumerable Token = ERC721Enumerable(tokenCA);
+        IERC721Enumerable token = IERC721Enumerable(_tokenCA);
 
-        return Token.ownerOf(tokenId);
-    }
-
-    function tokenURI(
-        address tokenCA,
-        uint256 tokenId
-    ) external view returns (string memory) {
-        ERC721Enumerable Token = ERC721Enumerable(tokenCA);
-
-        return Token.tokenURI(tokenId);
+        return token.ownerOf(_tokenId);
     }
 
     function tokensOfOwner(
         // Enumerable을 안쓴 NFT 컨트렉트는 이거 못함...
-        address tokenCA,
-        address owner
+        address _tokenCA,
+        address _owner
     ) external view returns (uint[] memory) {
-        ERC721Enumerable Token = ERC721Enumerable(tokenCA);
-        uint balance = Token.balanceOf(owner);
+        IERC721Enumerable token = IERC721Enumerable(_tokenCA);
+        uint balance = token.balanceOf(_owner);
         uint[] memory tokensOfOwnerList = new uint[](balance);
 
-        for (uint index = 0; index < balance; index++) {
-            tokensOfOwnerList[index] = Token.tokenOfOwnerByIndex(owner, index);
+        for (uint256 index = 0; index < balance; index++) {
+            tokensOfOwnerList[index] = token.tokenOfOwnerByIndex(_owner, index);
         }
 
         return tokensOfOwnerList;
+    }
+
+    // meta_data
+
+    function name(address _tokenCA) external view returns (string memory) {
+        IERC721Metadata token = IERC721Metadata(_tokenCA);
+
+        return token.name();
+    }
+
+    function symbol(address _tokenCA) external view returns (string memory) {
+        IERC721Metadata token = IERC721Metadata(_tokenCA);
+
+        return token.symbol();
+    }
+
+    function tokenURI(
+        address _tokenCA,
+        uint256 _tokenId
+    ) external view returns (string memory) {
+        IERC721Metadata token = IERC721Metadata(_tokenCA);
+
+        return token.tokenURI(_tokenId);
     }
 }
