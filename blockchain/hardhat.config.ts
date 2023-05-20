f478bad6b418d70be6b9cfa315aea6cf253b3d1e
ssups <shalstlqruki@gmail.com>
2023-05-21 02:56:44 +0900

    update: change to typescript

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -1,12 +1,15 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+import { HardhatUserConfig } from "hardhat/config";
 // require('@nomiclabs/hardhat-waffle');
 // test코드에서 solidity int 리턴이 bigInt로 되있어서 비교가 힘들었음
 // 그래서 waffle 에서 toolbox로 바꿈 공식문서에서도 추천
+import "@nomicfoundation/hardhat-toolbox";
 // https://hardhat.org/hardhat-runner/docs/guides/migrating-from-hardhat-waffle
-require('@nomicfoundation/hardhat-toolbox');
 // https://hardhat.org/hardhat-chai-matchers/docs/overview
-require('@nomicfoundation/hardhat-chai-matchers');
+import "@nomicfoundation/hardhat-chai-matchers";
+import "@openzeppelin/hardhat-upgrades";
 
-module.exports = {
-  solidity: '0.8.18',
+const config: HardhatUserConfig = {
+  solidity: "0.8.18",
 };
+
+export default config;
