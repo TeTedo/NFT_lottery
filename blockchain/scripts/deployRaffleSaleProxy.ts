871b8156025d76107444a82819622e2888b38b2e
ssups <shalstlqruki@gmail.com>
2023-05-28 01:19:28 +0900

    refactor: redeploy

--- blockchain/scripts/deployRaffleSaleProxy.ts
+++ blockchain/scripts/deployRaffleSaleProxy.ts
@@ -1,19 +1,19 @@
-import { config, ethers, network, upgrades } from 'hardhat';
-import { RaffleSale, RaffleSale__factory, TestToken } from '../typechain-types/index';
+import { config, ethers, network, upgrades } from "hardhat";
+import { RaffleSale, RaffleSale__factory, TestToken } from "../typechain-types/index";
 
 async function main() {
   const MAX_TICKET_AMOUNT = 1_000;
-  const MIN_TICKET_PRICE = ethers.utils.parseEther('0.0001');
+  const MIN_TICKET_PRICE = ethers.utils.parseEther("0.0001");
   const COMMISSION_PERCENTAGE = 10;
 
   const signer = (await ethers.getSigners())[0];
-  const contractFactory = await ethers.getContractFactory('RaffleSale', signer);
+  const contractFactory = await ethers.getContractFactory("Raffle", signer);
   const raffleSaleProxy = (await upgrades.deployProxy(
     contractFactory,
     [MAX_TICKET_AMOUNT, MIN_TICKET_PRICE, COMMISSION_PERCENTAGE],
     {
-      initializer: 'initialize',
-      unsafeAllow: ['constructor'],
+      initializer: "initialize",
+      unsafeAllow: ["constructor"],
     }
   )) as RaffleSale;
   await raffleSaleProxy.deployed();
@@ -26,14 +26,14 @@ async function main() {
   );
 }
 
-main().catch(error => {
+main().catch((error) => {
   console.log(error);
   process.exitCode = 1;
 });
 
 // ** gas used **
 // sepolia: {
-//   logic_contract: 3,180,375,
+//   logic_contract: 3,798,979,
 //   proxy_admin_contract: 443,289,
-//   proxy_contract: 750,751,
+//   proxy_contract: 750,751 | 750,742,
 // }
