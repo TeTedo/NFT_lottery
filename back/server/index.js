9aebf69efb409577da00008b4645b3a3dd85e636
TeTedo <qkrxotjr9508@gmail.com>
2023-05-29 11:00:47 +0900

    Delete : nodejs backend

--- back/server/index.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config({ path: "env/back.env" });
-const app = require("../app");
-const PORT = process.env.PORT;
-const env = process.env.NODE_ENV;
-app.listen(PORT, () => {
-  console.log(`server started at ${PORT} PORT in ${env} ENV`);
-});
