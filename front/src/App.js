36d739d279ab98c679b468e386bd9cb2dc3c9172
HanGyeol9931 <jn365@naver.com>
2023-06-10 15:45:44 +0900

    라우터 추가

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Page1 from "./pages/page1";
+import Page2 from "./pages/page2";
 
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Page1 />} />
+        <Route path="/page1" element={<Page1 />} />
+        <Route path="/page2" element={<Page2 />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
