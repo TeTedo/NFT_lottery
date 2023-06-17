99e0f3f8aea15c5c5119f265976510644e7aff3b
lcm991005 <lcm991005@gmail.com>
2023-06-17 16:42:17 +0900

    디테이 페이지 디자인 변경

---
+++ front/src/components/Product/MetaDataContent/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container } from "./styles";
+
+function ContentsDetail() {
+  // const items = {
+  //   address: "0x2358...a68b",
+  //   tokenID: "7883",
+  //   tokenStandard: "ERC-721",
+  //   blockchain: "Ethereum",
+  //   creatorFees: "5%",
+  // };
+  return <Container>MetaDataContent</Container>;
+}
+
+export default ContentsDetail;
---
+++ front/src/pages/ProductDetail/RightUpComponents.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { RightUp } from "./styles";
+
+function RightUpComponents() {
+  return (
+    <RightUp.Content>
+      <RightUp.Img>ProductDetail3</RightUp.Img>
+    </RightUp.Content>
+  );
+}
+
+export default RightUpComponents;
