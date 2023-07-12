471525a9e7b23b2fcfb5c901f8a308c4696c91be
TeTedo <qkrxotjr9508@gmail.com>
2023-07-13 00:41:03 +0900

    Chore : add KlaynService for event handle

--- back/src/main/java/com/undefined/undefined/global/web3/klaytn/listener/KlaytnEventListener.java
+++ back/src/main/java/com/undefined/undefined/global/web3/klaytn/listener/KlaytnEventListener.java
@@ -1,5 +1,8 @@
-package com.undefined.undefined.global.web3.klaytn;
+package com.undefined.undefined.global.web3.klaytn.listener;
 
+import com.undefined.undefined.global.web3.klaytn.service.KlaytnService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.web3j.abi.EventEncoder;
 import org.web3j.abi.FunctionReturnDecoder;
@@ -21,12 +24,15 @@ import java.util.List;
 @Component
 public class KlaytnEventListener {
 
+
     private static final String WEBSOCKET_URI = "wss://public-en-baobab.klaytn.net/ws";
     private static final String CONTRACT_ADDRESS = "0x381c7d85673f230de554097372e0FC4F14b5B0a6";
 
     private final WebSocketService webSocketService;
     private final Web3j web3j;
-    private Event event;
+
+    @Autowired
+    KlaytnService klaytnService;
 
     public KlaytnEventListener() throws ConnectException {
         webSocketService = new WebSocketService(WEBSOCKET_URI, false);
@@ -45,31 +51,7 @@ public class KlaytnEventListener {
 
     private void handledEvent(Log log) {
         String eventHash = log.getTopics().get(0);
-
-        event = new Event("Test", Arrays.<TypeReference<?>>asList(
-                new TypeReference<Uint>() {
-                },
-                new TypeReference<Utf8String>() {
-                },new TypeReference<Uint>() {
-                }
-              ));
-
-        String MY_EVENT_HASH = EventEncoder.encode(event);
-
-        if(eventHash.equals(MY_EVENT_HASH)){
-            List<Type> decodeData = FunctionReturnDecoder.decode(log.getData(), event.getNonIndexedParameters());
-
-            Uint _num1 = (Uint) decodeData.get(0);
-            Utf8String _str = (Utf8String) decodeData.get(1);
-            Uint _num2 = (Uint) decodeData.get(2);
-
-            System.out.println(decodeData.get(0).getValue());
-            System.out.println(_num1.getValue());
-            System.out.println(_str);
-            System.out.println(_num2.getValue());
-
-
-        }
+        klaytnService.balancer(log, eventHash);
     }
 }
 
