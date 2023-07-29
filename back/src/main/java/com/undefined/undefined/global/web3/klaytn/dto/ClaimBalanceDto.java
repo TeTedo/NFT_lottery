57c4177b3428c486a013baa1a7b2f0810d86c923
TeTedo <qkrxotjr9508@gmail.com>
2023-07-29 11:37:14 +0900

    Chore : claim balance, price By Event;

---
+++ back/src/main/java/com/undefined/undefined/global/web3/klaytn/dto/ClaimBalanceDto.java
@@ -0,0 +1,19 @@
+package com.undefined.undefined.global.web3.klaytn.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class ClaimBalanceDto {
+    private String claimer;
+    private double amount;
+    private double afterBalance;
+
+    // TODO afterBalance, price 필요 없어보임
+    @Builder
+    public ClaimBalanceDto(String claimer, double amount, double afterBalance) {
+        this.claimer = claimer;
+        this.amount = amount;
+        this.afterBalance = afterBalance;
+    }
+}
