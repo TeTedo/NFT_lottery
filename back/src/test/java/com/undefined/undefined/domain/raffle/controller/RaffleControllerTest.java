2f72446cd6bbd823de346d9a25f432b69315304c
TeTedo <qkrxotjr9508@gmail.com>
2023-07-31 21:44:46 +0900

    Test : add test - get all raffles

--- back/src/test/java/com/undefined/undefined/domain/raffle/controller/RaffleControllerTest.java
+++ back/src/test/java/com/undefined/undefined/domain/raffle/controller/RaffleControllerTest.java
@@ -1,10 +1,8 @@
 package com.undefined.undefined.domain.raffle.controller;
 
-import com.undefined.undefined.domain.collection.controller.CollectionController;
-import com.undefined.undefined.domain.collection.dto.response.CollectionResponse;
-import com.undefined.undefined.domain.collection.service.CollectionService;
+import com.undefined.undefined.domain.raffle.dto.request.GetAllRaffleListRequest;
 import com.undefined.undefined.domain.raffle.dto.request.GetMyRaffleListRequest;
-import com.undefined.undefined.domain.raffle.dto.response.MyRaffleResponse;
+import com.undefined.undefined.domain.raffle.dto.response.RaffleResponse;
 import com.undefined.undefined.domain.raffle.service.RaffleService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -26,7 +24,6 @@ import org.springframework.web.context.WebApplicationContext;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
@@ -63,11 +60,11 @@ public class RaffleControllerTest {
                 .address(address)
                 .build();
 
-        List<MyRaffleResponse> dummy = Arrays.asList(
-                MyRaffleResponse.builder().id(1L).ca("ca").tokenUri("tokenUri").tokenId(1).totalTicket(100).leftTicket(50).ticketPrice(0.1).endTime(LocalDateTime.now()).isEnd(true).isPaid(false).settlement(10).createdAt(LocalDateTime.now()).build()
+        List<RaffleResponse> dummy = Arrays.asList(
+                RaffleResponse.builder().id(1L).ca("ca").tokenUri("tokenUri").tokenId(1).totalTicket(100).leftTicket(50).ticketPrice(0.1).endTime(LocalDateTime.now()).isEnd(true).isPaid(false).settlement(10).createdAt(LocalDateTime.now()).build()
         );
 
-        Page<MyRaffleResponse> response = new PageImpl<>(dummy, pageable, size);
+        Page<RaffleResponse> response = new PageImpl<>(dummy, pageable, size);
 
         Mockito.when(raffleService.getMyRaffle(request))
                 .thenReturn(response);
@@ -95,6 +92,75 @@ public class RaffleControllerTest {
                                 fieldWithPath("content[].createdAt").description("Creation time of the raffle").type("string"),
                                 fieldWithPath("content[].end").description("Is the raffle ended").type("boolean"),
                                 fieldWithPath("content[].paid").description("Is the raffle paid").type("boolean"),
+                                fieldWithPath("content[].claimNft").description("Is the nft claimed").type("boolean"),
+                                fieldWithPath("totalElements").description("Total number of elements").type("Number"),
+                                fieldWithPath("totalPages").description("Total number of pages").type("Number"),
+                                fieldWithPath("pageable").description("Pagination information"),
+                                fieldWithPath("pageable.sort.empty").description("Is the sort empty").type("boolean"),
+                                fieldWithPath("pageable.sort.sorted").description("Is the sort sorted").type("boolean"),
+                                fieldWithPath("pageable.sort.unsorted").description("Is the sort unsorted").type("boolean"),
+                                fieldWithPath("pageable.offset").description("Offset of the page").type("number"),
+                                fieldWithPath("pageable.pageNumber").description("Page number").type("number"),
+                                fieldWithPath("pageable.pageSize").description("Page size").type("number"),
+                                fieldWithPath("pageable.paged").description("Is paged").type("boolean"),
+                                fieldWithPath("pageable.unpaged").description("Is unpaged").type("boolean"),
+                                fieldWithPath("last").description("Is the last page").type("boolean"),
+                                fieldWithPath("size").description("Number of elements in the current page").type("number"),
+                                fieldWithPath("number").description("Current page number").type("number"),
+                                fieldWithPath("sort.empty").description("Is the sort empty").type("boolean"),
+                                fieldWithPath("sort.sorted").description("Is the sort sorted").type("boolean"),
+                                fieldWithPath("sort.unsorted").description("Is the sort unsorted").type("boolean"),
+                                fieldWithPath("first").description("Is the first page").type("boolean"),
+                                fieldWithPath("numberOfElements").description("Total number of elements in the result").type("number"),
+                                fieldWithPath("empty").description("Is the result empty").type("boolean"),
+                                fieldWithPath("content").description("List of collection items").type("Array of CollectionResponse"))));
+    }
+
+    @Test
+    @DisplayName("GET /raffles?size=10&page=0")
+    void getAllRaffles() throws Exception{
+        // given
+        int size = 10;
+        Sort sort = Sort.by(Sort.Direction.DESC, "createdAt");
+        Pageable pageable = PageRequest.of(0,10, sort);
+        GetAllRaffleListRequest request = GetAllRaffleListRequest.builder()
+                .pageable(pageable)
+                .build();
+
+        List<RaffleResponse> dummy = Arrays.asList(
+                RaffleResponse.builder().id(1L).ca("ca").tokenUri("tokenUri").tokenId(1).totalTicket(100).leftTicket(50).ticketPrice(0.1).endTime(LocalDateTime.now()).isEnd(true).isPaid(false).settlement(10).createdAt(LocalDateTime.now()).build()
+        );
+
+        Page<RaffleResponse> response = new PageImpl<>(dummy, pageable, size);
+
+        Mockito.when(raffleService.getAllRaffles(request))
+                .thenReturn(response);
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.get("/raffles?page=0")
+                        .param("size", "10"));
+
+        // then
+        Mockito.verify(raffleService).getAllRaffles(request);
+
+        resultActions.andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcRestDocumentation.document("allRaffles",
+                        responseFields(
+                                fieldWithPath("content").description("Array of MyRaffleResponse objects"),
+                                fieldWithPath("content[].id").description("id").type("Long"),
+                                fieldWithPath("content[].ca").description("contract address").type("String"),
+                                fieldWithPath("content[].tokenId").description("id").type("number"),
+                                fieldWithPath("content[].tokenUri").description("tokenUri").type("string"),
+                                fieldWithPath("content[].totalTicket").description("Total number of tickets").type("number"),
+                                fieldWithPath("content[].leftTicket").description("Number of remaining tickets").type("number"),
+                                fieldWithPath("content[].ticketPrice").description("Ticket price").type("number"),
+                                fieldWithPath("content[].endTime").description("End time of the raffle").type("string"),
+                                fieldWithPath("content[].settlement").description("Settlement amount").type("number"),
+                                fieldWithPath("content[].createdAt").description("Creation time of the raffle").type("string"),
+                                fieldWithPath("content[].end").description("Is the raffle ended").type("boolean"),
+                                fieldWithPath("content[].paid").description("Is the raffle paid").type("boolean"),
+                                fieldWithPath("content[].claimNft").description("Is the nft claimed").type("boolean"),
                                 fieldWithPath("totalElements").description("Total number of elements").type("Number"),
                                 fieldWithPath("totalPages").description("Total number of pages").type("Number"),
                                 fieldWithPath("pageable").description("Pagination information"),
