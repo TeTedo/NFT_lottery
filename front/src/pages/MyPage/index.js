33c602aaf048bba4592fd3ec95b828e0c96366fa
HanGyeol9931 <jn365@naver.com>
2023-08-18 17:01:10 +0900

    프론트 작업물

--- front/src/pages/MyPage/index.js
+++ front/src/pages/MyPage/index.js
@@ -1,14 +1,24 @@
 import React from "react";
-import { Container, Contents } from "./styles";
+import { Container, Contents , Left,Right} from "./styles";
+import ProductList from "components/Main/ProductList";
 
 function MyPage() {
   return (
     <>
       <Container>
         <Contents.Container>
-            <Contents.Menu>
-                
-            </Contents.Menu>
+                <Left.Container>
+                  <Left.Title>My Page</Left.Title>
+                  <Left.Content>보유</Left.Content>
+                  <Left.Content>판매</Left.Content>
+                </Left.Container>
+                <Right.Container>
+                    <Right.Menu>
+                      <Right.Item>NFT</Right.Item>
+                      <Right.Item>TICKET</Right.Item>
+                    </Right.Menu>
+                    <ProductList />
+                </Right.Container>
         </Contents.Container>
       </Container>
     </>
--- front/src/pages/MyPage/styles.js
+++ front/src/pages/MyPage/styles.js
@@ -12,7 +12,7 @@ const Container = styled.div`
 `;
 const Contents = {
   Container: styled.div`
-    border: 1px solid white;
+    /* border: 1px solid white; */
     margin-top: 130px;
     width: 1400px;
     padding-bottom: 110px;
@@ -23,10 +23,46 @@ const Contents = {
     width: 300px;
     border: 1px solid white;
   `,
-  Menu: styled.div`
-    flex: 1;
-    width: 300px;
-    border: 1px solid white;
-  `,
 };
-export { Container, Contents };
+const Left ={
+  Container : styled.div`
+  
+  width: 250px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+  `,
+  Title : styled.div`
+  margin-top: 50px;
+  color: ${colors.white};
+  font-size: 20px;
+  `,
+  Content : styled.div`
+  margin-left: 20px;
+  color: ${colors.white};
+  font-size: 16px;  
+  `
+
+  
+}
+const Right ={
+  Container: styled.div`
+  height: 100%;
+  flex: 1;
+  `,
+  Menu: styled.div`
+  display: flex;
+  gap: 20px;
+  padding-bottom: 25px;
+  `,
+  Item: styled.div`
+  font-size: 16px;
+  border: 1px solid white;
+  padding: 10px 20px;
+  border-radius: 30px;
+  color: ${colors.white};
+  `
+}
+
+export { Container, Contents , Left, Right};
