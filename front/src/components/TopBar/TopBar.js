f0cd8a06a33f62a07c02a22e848390344f7e2254
HanGyeol9931 <jn365@naver.com>
2023-08-22 21:13:14 +0900

    메타마스크 지갑 연결

--- front/src/components/TopBar/TopBar.js
+++ front/src/components/TopBar/TopBar.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { Contents } from "./styles";
 import { useNavigation } from "hooks/useNavigation";
+// import useWeb3 from "hooks/useWeb3";
+import useConnectMetaMask from "hooks/useWeb3";
 
 export function TopBar() {
   const { goToHome, goToMyPage } = useNavigation();
+  const { connect, accounts, isConnected } = useConnectMetaMask();
+  console.log(accounts);
+  console.log(isConnected);
   return (
     <Contents.Container>
       <Contents.Content>
@@ -11,7 +16,15 @@ export function TopBar() {
         <Contents.TitleContainer>
           <Contents.Title onClick={goToMyPage}>My Page</Contents.Title>
         </Contents.TitleContainer>
-        <Contents.Login>Login</Contents.Login>
+        {!isConnected ? (
+          <>
+            <button onClick={() => connect("metamask")}>
+              Connect with MetaMask
+            </button>
+          </>
+        ) : (
+          <div>연결 계정 :  {accounts[0]}</div>
+        )}
       </Contents.Content>
     </Contents.Container>
   );
