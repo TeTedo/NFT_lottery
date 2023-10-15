0b31d00c951d063a092f0c4432d7686d0c0de9d3
박태석 <qkrxotjr9508@gmail.com>
2023-10-15 22:59:11 +0900

    refactor : using objectMapper when convert

--- back/src/main/java/com/undefined/undefined/domain/collection/mapper/CollectionMapper.java
+++ back/src/main/java/com/undefined/undefined/domain/collection/mapper/CollectionMapper.java
@@ -1,22 +1,21 @@
 package com.undefined.undefined.domain.collection.mapper;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.undefined.undefined.domain.collection.dto.response.CollectionResponse;
 import com.undefined.undefined.domain.collection.model.Collection;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class CollectionMapper {
-    public CollectionResponse toCollectionResponse(Collection collection){
-        return CollectionResponse.builder()
-                .id(collection.getId())
-                .ca(collection.getContractAddress())
-                .name(collection.getContractName())
-                .tokenUri(collection.getTokenUri())
-                .build();
-    }
+    private final ObjectMapper objectMapper;
 
+    public CollectionResponse toCollectionResponse(Collection collection){
+        return objectMapper.convertValue(collection, CollectionResponse.class);
+    }
     public Page<CollectionResponse> toResponse(Page<Collection> collectionPage) {
-        return collectionPage.map(v-> toCollectionResponse(v));
+        return collectionPage.map(this::toCollectionResponse);
     }
 }
